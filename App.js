b48f85c878ce83d5563114ff889baee80c0ccba0
Siva Rajan <citysiva180@gmail.com>
2022-01-30 09:23:57 +0530

    completed - journalling and flex

--- App.js
+++ App.js
@@ -3,24 +3,46 @@ import { View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
+    <View
+      style={{
+        backgroundColor: "white",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "baseline", //main axis is horizontal axis
+      }}
+    >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: "100%",
-          height: landscape ? "100%" : "30%",
+          width: 100,
+          height: 300,
+          // alignSelf: "flex-start",
         }}
       ></View>
-    </SafeAreaView>
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 200,
+        }}
+      ></View>
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      ></View>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "white",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
